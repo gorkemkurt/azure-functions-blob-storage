69af33564833b74d5ef4c8c6b1d17adabe71ec40
Gorkem Kurt <gorkem@elephantsdontforget.com>
2020-04-18 18:57:44 +0300

    tests addded

--- src/test/java/com/function/FunctionTest.java
+++ src/test/java/com/function/FunctionTest.java
@@ -1,43 +1,103 @@
 package com.function;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-
-/**
- * Unit test for Function class.
- */
 public class FunctionTest {
-    /**
-     * Unit test for HttpTriggerJava method.
-     */
     @Test
-    public void testHttpTriggerJava() throws Exception {
-        // // Setup
-        // @SuppressWarnings("unchecked")
-        // final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+    public void testCreateFile() throws Exception {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<FileRequest>> req = mock(HttpRequestMessage.class);
 
-        // final Map<String, String> queryParams = new HashMap<>();
-        // queryParams.put("name", "Azure");
-        // doReturn(queryParams).when(req).getQueryParameters();
+        final Optional<String> queryBody = Optional.empty();
+        doReturn(queryBody).when(req).getBody();
 
-        // final Optional<String> queryBody = Optional.empty();
-        // doReturn(queryBody).when(req).getBody();
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(final InvocationOnMock invocation) {
+                final HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
 
-        // doAnswer(new Answer<HttpResponseMessage.Builder>() {
-        //     @Override
-        //     public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
-        //         HttpStatus status = (HttpStatus) invocation.getArguments()[0];
-        //         return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
-        //     }
-        // }).when(req).createResponseBuilder(any(HttpStatus.class));
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
 
-        // final ExecutionContext context = mock(ExecutionContext.class);
-        // doReturn(Logger.getGlobal()).when(context).getLogger();
+        @SuppressWarnings("unchecked")
+        final OutputBinding<byte[]> outputItem = (OutputBinding<byte[]>) mock(OutputBinding.class);
 
-        // // Invoke
-        // final HttpResponseMessage ret = new Function().run(req, context);
+        final HttpResponseMessage ret = new Function().createFile(req, "name", "content", outputItem, context);
 
-        // // Verify
-        // assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+    }
+
+    @Test
+    public void testAddFileToQueue_ReturnBadRequest_WhenRequestBodyEmpty() throws Exception {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<FileRequest>> req = mock(HttpRequestMessage.class);
+
+        final Optional<String> queryBody = Optional.empty();
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(final InvocationOnMock invocation) {
+                final HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        @SuppressWarnings("unchecked")
+        final OutputBinding<FileRequest> outputItem = (OutputBinding<FileRequest>) mock(OutputBinding.class);
+
+        final HttpResponseMessage ret = new Function().addFileToQueue(req, outputItem, context);
+
+        assertEquals(ret.getStatus(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testAddFileToQueue() throws Exception {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<FileRequest>> req = mock(HttpRequestMessage.class);
+
+        final Optional<String> queryBody = Optional.of("json");
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(final InvocationOnMock invocation) {
+                final HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        @SuppressWarnings("unchecked")
+        final OutputBinding<FileRequest> outputItem = (OutputBinding<FileRequest>) mock(OutputBinding.class);
+
+        final HttpResponseMessage ret = new Function().addFileToQueue(req, outputItem, context);
+
+        assertEquals(ret.getStatus(), HttpStatus.OK);
     }
 }
